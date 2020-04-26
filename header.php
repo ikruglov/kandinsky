9420d4dd7762c28e91a2e0b86194ca69432aff32
Ivan Kruglov <mail@ikruglov.com>
2020-04-27 00:18:19 +0200

    fix mixed content

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="profile" href="https://gmpg.org/xfn/11">
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 <?php wp_head(); ?>
